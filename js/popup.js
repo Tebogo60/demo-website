baa8442cbbfc4c58b17eeee316bb26244fd89d6b
tebogo60 <tjjmalebana@gmail.com>
2024-10-27 11:17:42 +0200

    removed unused files

--- js/popup.js
+++
@@ -1,6 +0,0 @@
-function togglePopupLi(){
-    document.getElementById("popup-1").classList.toggle("active");
-}
-function togglePopupSu(){
-    document.getElementById("popup-2").classList.toggle("active");
-}
